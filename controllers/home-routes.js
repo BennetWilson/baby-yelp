4024cd12d29edfc0caadf596f7c7c8005b8380c0
Bennet Wilson <bennetwilson@gmail.com>
2022-01-26 14:43:16 -0700

    changed home routes to basically redirects

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,22 @@
 const router = require("express").Router();
-const { Restaurant, Review, User } = require("../models");
-const withAuth = require("../utils/auth");
+// const { Restaurant, Review, User } = require("../models");
+// const withAuth = require("../utils/auth");
 
+// Maybe redirect to restaurants and reviews.
+router.get('/restaurant', (req, res) => {
+    if (req.res.logged_in) {
+        res.redirect('./api/restaurant-routes');
+        return;
+    }
+    res.render('restaurant'); 
+});
 
+router.get('/reviews', (req, res) => {
+    if(req.res.logged_in) {
+        res.redirect('./api/review-routes');
+        return;
+    }
+    res.render('reviews');
+})
 
 module.exports = router;
