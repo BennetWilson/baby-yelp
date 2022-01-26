297984d9c4681f1cfdf852c644f8d4d50a782839
Tgarrey37 <tpopp0303@gmail.com>
2022-01-26 12:18:55 -0700

    Model Index done

---
+++ models/index.js
@@ -0,0 +1,23 @@
+const User = require('./User');
+const Review = require('./Review');
+const Restaurant = require('./Restaurant');
+
+
+User.hasMany(Review, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Review.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Restaurant.belongsTo(User,{
+    foreignKey: 'user_id'
+});
+
+Review.belongsTo(Restaurant,{
+    foreignKey: 'restaurant_id'
+})
+
+module.exports = {User, Review, Restaurant};
