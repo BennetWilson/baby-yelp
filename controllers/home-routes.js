496a35b8441c8e4c2e841cefd8747d1cf49a35c5
Jasper Oudemans <jasperoudemans@gmail.com>
2022-01-27 14:02:43 -0700

    changes to handlebars files and to homeroutes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const { Restaurant, Review, User } = require("../models");
 
-
 // Maybe redirect to restaurants and reviews.
 const withAuth = require("../utils/auth");
 
@@ -10,64 +9,65 @@ router.get("/", async (req, res) => {
     // Get all projects and JOIN with user data
     const restaurantData = await Restaurant.findAll({
       attributes: ["id", "restaurant_name", "description", "created_at"],
-      include: [{
-        model: Review,
-        attributes: ['id', 'title', 'text', 'user_id', 'created_at'],
-        include: {
+      include: [
+        {
+          model: Review,
+          attributes: ["id", "title", "text", "user_id", "created_at"],
+          include: {
             model: User,
-            attributes: ['username']
-        }
-    },
-    {
-        model: User,
-        attributes: ['username']
-    }
-]
-})
-const restaurants = restaurantData.map((post) => post.get({ plain: true }));
+            attributes: ["username"],
+          },
+        },
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
+    const restaurants = restaurantData.map((post) => post.get({ plain: true }));
 
-res.render('homepage', {
-    restaurants, logged_in: req.session.logged_in,
-});
-
-  } catch (err){
-      res.status(500).json(err);
+    res.render("login", {
+      restaurants,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 router.get("/login", (req, res) => {
-    if (req.session.logged_in) {
-        res.redirect("dashboard");
-        return;
-    }
-    res.render('login');
-   });
-   
-   router.get('/signup',(req,res)=>{
-       if(req.session.logged_in){
-           res.redirect('dashboard');
-           return;
-       }
-       res.render('signup');
-   });
+  if (req.session.logged_in) {
+    res.redirect("dashboard");
+    return;
+  }
+  res.render("login");
+});
 
-router.get('/restaurant/:id', withAuth, async (req, res) => {
-    const restaurantData = await Restaurant.findByPk({
-        where: {
-            id: req.params.id,
-        },
-        include: [
-            User,
-            {
-                model: Review,
-                include: [User]
-            }
-        ],
-    })
-    if(req.res.logged_in) {
-        res.redirect('./api/review-routes');
-        return;
-    }
-    res.render('reviews');
-})
+router.get("/signup", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("dashboard");
+    return;
+  }
+  res.render("signup");
+});
+
+router.get("/restaurant/:id", withAuth, async (req, res) => {
+  const restaurantData = await Restaurant.findByPk({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      User,
+      {
+        model: Review,
+        include: [User],
+      },
+    ],
+  });
+  if (req.res.logged_in) {
+    res.redirect("./api/review-routes");
+    return;
+  }
+  res.render("reviews");
+});
 
 module.exports = router;
